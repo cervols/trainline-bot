43ff7a199cade9a5f2b8897c4bea12d0dfbc93c3
cervols <paprikamail@gmail.com>
2023-11-29 19:32:59 +0100

    Convert prices to default currency

--- lib/auto/com_thetrainline/fare.rb
+++ lib/auto/com_thetrainline/fare.rb
@@ -1,5 +1,7 @@
 module ComThetrainline
   module Fare
+    DEFAULT_CURRENCY = 'USD'
+
     module_function
 
     def parse(fares, section, alternatives)
@@ -16,8 +18,8 @@ module ComThetrainline
       fare_leg = fare[:fareLegs].first
 
       {
-        price_in_cents: price_in_cents(price),
-        currency: currency,
+        price_in_cents: price_in_cents(price, currency),
+        currency: DEFAULT_CURRENCY,
         name: fare_leg.dig(:travelClass, :name),
         comfort_class: fare_leg.dig(:comfort, :name)
       }
@@ -29,8 +31,31 @@ module ComThetrainline
       alternatives[cheapest_alternative_id]
     end
 
-    def price_in_cents(price)
-      (price * 100).to_i
+    def price_in_cents(price, currency)
+      return (price * 100).to_i if default_currency?(currency)
+
+      setup_money_exchange_rates
+
+      convert_to_default_currency(price, currency)
+    end
+
+    def default_currency?(currency)
+      currency == DEFAULT_CURRENCY
+    end
+
+    def setup_money_exchange_rates
+      return if Money.default_bank.is_a?(EuCentralBank)
+
+      eu_bank = EuCentralBank.new
+      eu_bank.update_rates
+
+      Money.default_bank = eu_bank
+    end
+
+    def convert_to_default_currency(price, currency)
+      Money.from_amount(price, currency).exchange_to(DEFAULT_CURRENCY).cents
+    rescue Money::Currency::UnknownCurrency, Money::Bank::UnknownRate
+      0
     end
   end
 end
